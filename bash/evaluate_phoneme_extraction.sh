fc290c7da4da36293612dc9befcfe2d8ebba07a5
Lorenz Bernard Marqueses <lorenz_marqueses@dlsu.edu.ph>
2026-01-30 11:47:51 +0800

    Reduce SentencePiece vocab size to 34

--- bash/evaluate_phoneme_extraction.sh
+++ bash/evaluate_phoneme_extraction.sh
@@ -1,5 +1,5 @@
 uv run src/scripts/nemo/speech_to_text_eval.py \
-    model_path="models/checkpoints/stt_en_conformer_val_wer-0.4321.nemo" \
+    model_path="nemo_experiments/Speech_To_Text_Finetuning/2026-01-30_00-04-12/checkpoints/Speech_To_Text_Finetuning.nemo" \
     dataset_manifest="data/nexdata/filipino_822/test_manifest.json" \
     batch_size=16 \
     amp=True \
--- bash/train_tokenizer.sh
+++ bash/train_tokenizer.sh
@@ -1,7 +1,7 @@
 uv run src/scripts/nemo/process_asr_text_tokenizer.py \
     --manifest=data/nexdata/filipino_822/train_manifest.json \
     --data_root=models/tokenizers/ipa_tokenizer \
-    --vocab_size=120 \
+    --vocab_size=34 \
     --tokenizer=spe \
     --spe_type=char \
     --spe_character_coverage=1.0 \
